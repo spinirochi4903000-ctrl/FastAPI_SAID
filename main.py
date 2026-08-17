ece6c2694a61709b083a1772aed17c872e0fc6ad
spinirochi4903000-ctrl <spinirochi4903000@gmail.com>
2026-08-17 17:56:01 -0500

    Implementacion Html

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
 from model.user_connection import UserConnection
 from schema.user_schema import UserSchema
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+conn = UserConnection()
+# ... el resto de tu código igual
+
 conn = UserConnection()
 @app.get("/")
 def read_root():
